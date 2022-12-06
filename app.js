63c249a375f7583ee1faee3a57d66d8ea61c5d8b
Sergey Smirnov <cepera.cMupHoB@gmail.com>
2022-12-06 06:41:19 +0200

    fix

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.get("/api/currency/nbu", async (req, res) => {
   const response = await fetch(url);
   const data = await response.text();
 
-  res.send(`<h2>++++++++++++++++</h2><div>${data}</div> `);
+  res.send(`<div>${data}</div> `);
 });
 
 module.exports = app;
